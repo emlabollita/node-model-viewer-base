c9926463be7922c08bc005e72f9d54a9dddffae1
Eduardo Martin Labollita <eduardo.martin.labollita@gmail.com>
2020-10-27 17:33:26 -0300

    nodejs init

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+//settings
+app.set("port", 4000);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//middlewares
+
+//routes
+app.use(require("./routes/"));
+
+//static files
+app.use(express.static(path.join(__dirname, "public")));
+
+//listening the port
+app.listen(app.get("port"), () => {
+  console.log("Server on port", app.get("port"));
+});
